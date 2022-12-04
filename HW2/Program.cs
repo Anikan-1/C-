eb50d42acb7d1ed38849a70c9d39c9ba1fe68d13
Anikanova <anikan1908@gmail.com>
2022-12-04 22:16:04 +0300

    Home work 2 done

---
+++ HW2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// void SecondDigit(int a){
+//     int dec = a/10%10;
+//     Console.WriteLine(dec);
+// }
+
+// Console.WriteLine("Введите трехзначное число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Результат - ");
+// SecondDigit (num1);
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// void ThirdDigit(int a){
+//    if (a>99){
+//     string b = a.ToString();
+//     Console.WriteLine(b[2]);
+//    }
+//     else {
+//         Console.WriteLine("В числе нет третьей цифры");
+//     }
+// }
+
+// Console.WriteLine("Введите число ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Результат - ");
+// ThirdDigit (num2);
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// void Weekend(int a){
+//     if (a == 6 ^ a == 7) {
+//         Console.WriteLine("Выходной день");
+//     }
+//     else {
+//         Console.WriteLine("Рабочий день");
+//     }
+// }
+
+// Console.WriteLine("Введите номер дня недели");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Результат - ");
+// Weekend (num3);
---
+++ Sem2/Program.cs
@@ -0,0 +1,61 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+// void Test(){
+//     int num = new Random().Next(100, 1000);
+//     Console.WriteLine($"Случайно число - {num}");
+//     int sot = num/100;
+//     int ed = num%10;
+//    Console.Write(sot);
+//    Console.WriteLine(ed);
+// }
+
+// Test ();
+
+
+// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления
+
+// void Test1(int a, int b){
+//    if (b%a == 0)
+//    {
+//     Console.WriteLine($"Число {b} кратно числу {a} ");
+//    }
+//     else {
+//         Console.WriteLine($"Остаток от деления {b%a}");
+//     }
+// }
+
+// Console.WriteLine("Введите первое число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Результат - ");
+// Test1 (num1, num2);
+
+//Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+//5, 25 -> да
+//-4, 16 -> да
+//25, 5 -> да
+//8,9 -> нет
+
+
+void Test2(int a, int b){
+   if (a == b*b)
+   {
+    Console.WriteLine($"Число {a} квадрат числа {b} ");
+   }
+   else if (b == a*a) {
+    Console.WriteLine($"Число {b} квадрат числа {a} ");
+   }
+    else {
+        Console.WriteLine("Числа не являются квадратами друг друга");
+    }
+}
+
+Console.WriteLine("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Результат - ");
+Test2 (num1, num2);

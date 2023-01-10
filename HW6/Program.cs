75927d00f70f3fc0fe13eb3b27977859d9405d12
Anikanova <anikan1908@gmail.com>
2023-01-10 21:38:34 +0300

    Add sem7

--- HW6/Program.cs
+++ HW6/Program.cs
@@ -2,57 +2,57 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-// int [] CreateArray (int num){
-//     int [] array = new int[num];
-//     for (int i = 0; i < num; i++)
-//     {
-//         System.Console.Write("Введите число: ");
-//         array[i] = Convert.ToInt32(Console.ReadLine());
-//     }
-//     return array; 
-// }
+int [] CreateArray (int num){
+    int [] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        System.Console.Write("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array; 
+}
 
-// void ShowArray (int [] array){
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write($"{array[i]} ");
-//     }
-//     Console.WriteLine();
-// }
+void ShowArray (int [] array){
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
 
-// void CountPos (int [] array) {
-//      int count = 0;
-//      for (int i = 0; i < array.Length; i++)
-//      {
-//          if (array[i] > 0) count++;
-//      }
-//      System.Console.WriteLine($"Положительных чисел: {count}");
-// }
+void CountPos (int [] array) {
+     int count = 0;
+     for (int i = 0; i < array.Length; i++)
+     {
+         if (array[i] > 0) count++;
+     }
+     System.Console.WriteLine($"Положительных чисел: {count}");
+}
 
-// System.Console.Write("Сколько нужно чисел: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int [] array = CreateArray(num);
-// ShowArray(array);
-// CountPos(array);
+System.Console.Write("Сколько нужно чисел: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int [] array = CreateArray(num);
+ShowArray(array);
+CountPos(array);
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-void FindIntersection (double k1, double b1, double k2, double b2)
-{
-    double x = (b2 - b1)/(k1 - k2);
-    double y = k1 * x + b1;
-    System.Console.WriteLine($"Точка пересечения прямых: ({x}; {y})");
-}
+// void FindIntersection (double k1, double b1, double k2, double b2)
+// {
+//     double x = (b2 - b1)/(k1 - k2);
+//     double y = k1 * x + b1;
+//     System.Console.WriteLine($"Точка пересечения прямых: ({x}; {y})");
+// }
 
-System.Console.Write("Введите k1: ");
-int k1 = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите b1: ");
-int b1 = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите k2: ");
-int k2 = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите b2: ");
-int b2 = Convert.ToInt32(Console.ReadLine());
-FindIntersection (k1, b1, k2, b2);
+// System.Console.Write("Введите k1: ");
+// int k1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите b1: ");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите k2: ");
+// int k2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите b2: ");
+// int b2 = Convert.ToInt32(Console.ReadLine());
+// FindIntersection (k1, b1, k2, b2);
---
+++ Sem7/Program.cs
@@ -0,0 +1,176 @@
+﻿// Задать двумерный масси m,n заполнить случайныи целыми ячислами
+
+// int [,] Create2dArray(int rows, int cols, int min, int max){
+//     int [,] array = new int [rows, cols];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < cols; j++)
+//         {
+//             array[i,j] = new Random().Next(min,max+1);
+//         }
+//     }
+//     return array;
+// }
+
+// void Show2dArray (int [,] array){
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//            Console.Write(array[i,j]+" ");
+//         }
+//         Console.WriteLine();
+//     }
+    
+// }
+
+// Console.Write("Введите количество строк: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите min элемент: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите max элемент: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// int [,] array = Create2dArray(m,n,min,max);
+// Show2dArray(array);
+
+// Задать двумерный масси m,n. Каждый элемент массива находится по формуле m+n
+
+// int [,] Create2dArray(int rows, int cols){
+//     int [,] array = new int [rows, cols];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < cols; j++)
+//         {
+//             array[i,j] = i + j;
+//         }
+//     }
+//     return array;
+// }
+
+// void Show2dArray (int [,] array){
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//            Console.Write(array[i,j]+" ");
+//         }
+//         Console.WriteLine();
+//     }
+    
+// }
+
+// Console.Write("Введите количество строк: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int [,] array = Create2dArray(m,n);
+// Show2dArray(array);
+
+// Задать двумерный массив
+// Найти элементы, у которых оба индекса четные и заменить элементы на их квадраты
+
+// int [,] Create2dArray(int rows, int cols, int min, int max){
+//     int [,] array = new int [rows, cols];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < cols; j++)
+//         {
+//             array[i,j] = new Random().Next(min,max+1);
+//         }
+//     }
+//     return array;
+// }
+
+// void Show2dArray (int [,] array){
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//            Console.Write(array[i,j]+" ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int [,] FindQuaters(int [,] array){
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//            if (i%2 == 0 && j%2 == 0){
+//             array[i,j] = array[i,j]*array[i,j];
+//            }
+//         }
+//     }
+//     return array;
+// }
+
+// Console.Write("Введите количество строк: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите min элемент: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите max элемент: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// int [,] array = Create2dArray(m,n,min,max);
+// Show2dArray(array);
+// FindQuaters(array);
+// Console.WriteLine();
+// Show2dArray(array);
+
+
+// ЗАДАЙТЕ ЛВУМЕРНЫЙ МАССИВ
+//  Найдите сумму элементов, находящихся на главной диагонали
+// с индексами (0;0),(1,1) итд
+
+int [,] Create2dArray(int rows, int cols, int min, int max){
+    int [,] array = new int [rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i,j] = new Random().Next(min,max+1);
+        }
+    }
+    return array;
+}
+
+void Show2dArray (int [,] array){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SumEl(int [,] array){
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           if (i == j){
+            sum += array[i,j];
+           }
+        }
+    }
+    System.Console.WriteLine("Сумма элементов главной диагонали: " + sum);
+}
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите min элемент: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите max элемент: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int [,] array = Create2dArray(m,n,min,max);
+SumEl(array);
+Show2dArray(array);
